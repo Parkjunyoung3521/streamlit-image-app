d88c7e1bd2bd86cb278bba3be87bd225df304a85
Parkjunyoung3521 <junyong3521@naver.com>
2021-03-10 10:22:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image, ImageFilter, ImageEnhance
 import os
 from datetime import datetime
-
+# 깃 연동
 def load_image(image_file) :
     img = Image.open(image_file)
     return img
